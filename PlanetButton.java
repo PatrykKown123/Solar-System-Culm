3824747296b8d1385fa47ec8a474e2b566bb2516
Anton-299 <34448137+Anton-299@users.noreply.github.com>
2017-12-11 09:01:25 -0500

    Add files via upload

---
+++ PlanetButton.java
@@ -0,0 +1,62 @@
+import java.awt.*;
+import java.awt.geom.*;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class PlanetButton extends JButton {
+
+	private Color buttonColor;
+
+	public PlanetButton(String label, Color c, int x, int y, int width, int height) {
+		super(label);
+
+		buttonColor = c;
+		// Inputs the width and height into the constructor of Dimension class
+		Dimension size = new Dimension(width, height);
+		// Sets the size of the button
+		setPreferredSize(size);
+
+		// Sets where the button will be and its width & height
+		setBounds(x, y, width, height);
+
+		// This fills the circular button and make it clickable, if it was true, then it
+		// would make the square button clickable and it would be seen
+		setContentAreaFilled(false);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		// If the button is clicked it turns light Grey, if not then it stays as the
+		// color of the programmers choice
+		if (getModel().isArmed()) {
+			g.setColor(Color.lightGray);
+		} else {
+			g.setColor(buttonColor);
+		}
+
+		// Draws the circle button and gets the width and height of the button
+		g.fillOval(0, 0, getSize().width - 1, getSize().height - 1);
+
+		// Calls the parent's paint logic to draw the component
+		super.paintComponent(g);
+	}
+
+	// Based on the button's correct circumference, this paints the border of it
+	// It gets the width and height from "size" from the constructor in this class
+	@Override
+	protected void paintBorder(Graphics g) {
+		g.drawOval(0, 0, getSize().width - 1, getSize().height - 1);
+	}
+
+	Shape shape;
+
+	// This method determines if the user missed the button or has clicked it
+	// correctly. It calculates if the button was clicked.
+	@Override
+	public boolean contains(int x, int y) {
+		if (shape == null) {
+			shape = new Ellipse2D.Float(0, 0, getWidth(), getHeight());
+		}
+		return shape.contains(x, y);
+	}
+}
---
+++ TestJFrame.java
@@ -0,0 +1,48 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TestJFrame {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame("Anton's JFrame.");
+		PlanetButton sun = new PlanetButton("Sun", Color.yellow, 0, 300, 250, 250);
+		PlanetButton mercury = new PlanetButton("Mercury", Color.gray, 300, 408, 35, 35);
+		PlanetButton venus = new PlanetButton("Venus", Color.DARK_GRAY, 380, 397, 55, 55);
+		PlanetButton earth = new PlanetButton("Earth", Color.BLUE, 490, 395, 60, 60);
+		PlanetButton mars = new PlanetButton("Mars", Color.red, 600, 403, 45, 45);
+		PlanetButton jupiter = new PlanetButton("Jupiter", Color.cyan, 720, 377, 100, 100);
+		PlanetButton saturn = new PlanetButton("Saturn", Color.YELLOW, 900, 383, 90, 90);
+		PlanetButton uranus = new PlanetButton("Uranus", Color.blue, 1100, 390, 75, 75);
+		PlanetButton neptune = new PlanetButton("Neptune", Color.blue, 1300, 387, 75, 75);
+		JLabel lab = new JLabel("This is the Sun");
+		JPanel p = new JPanel();
+		Container c = f.getContentPane();
+
+		f.setSize(new Dimension(1500, 800));
+		f.setLocationRelativeTo(null);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		p.setLayout(null);
+
+		p.add(sun);
+		p.add(mercury);
+		p.add(venus);
+		p.add(earth);
+		p.add(mars);
+		p.add(jupiter);
+		p.add(saturn);
+		p.add(uranus);
+		p.add(neptune);
+		p.add(lab);
+
+		f.add(p);
+		p.setBackground(Color.BLACK);
+
+		f.setVisible(true);
+	}
+}
